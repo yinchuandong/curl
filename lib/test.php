8503e3731afaf11253db112d7b6f5d3fbb93550e
yinchuandong <yincd520@sina.com>
2013-10-06 23:40:15 +0800

    增加华工的续借和借阅历史

--- lib/test.php
+++ lib/test.php
@@ -6,7 +6,8 @@ header('Content-type:text/html; charset=utf-8');
 $lib = Factory::createClass('LibHg');
 if($lib->checkField("D1130580120", "801211")){
 
-	var_dump($lib->renew('C2010604094'));
+// 	var_dump($lib->renew('C2010604094'));
+	var_dump($lib->getLoanList());
 
 }
 // if($lib->checkField("20111003632", "yin543211")){
