d125aeb207a1d56924339f4fdab449c4ec0b4d0e
yinchuandong <yincd520@sina.com>
2013-10-07 16:49:08 +0800

    暨南大学--》有问题

--- lib/test.php
+++ lib/test.php
@@ -3,12 +3,14 @@
 include 'Factory.php';
 
 header('Content-type:text/html; charset=utf-8');
-$lib = Factory::createClass('LibHg');
-if($lib->checkField("D1130580120", "801211")){
+$lib = Factory::createClass('LibJndx');
+if($lib->checkField("2011050663", "050663")){
 
 // 	var_dump($lib->renew('C2010604094'));
-	var_dump($lib->getLoanList());
-
+	var_dump($lib->getHistoryList());
+	echo 1;
+}else{
+	echo 0;
 }
 // if($lib->checkField("20111003632", "yin543211")){
 
