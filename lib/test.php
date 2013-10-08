dfe89b92c9c99d951075d3043156a7bb457d8ba9
yinchuandong <yincd520@sina.com>
2013-10-08 16:31:06 +0800

    confict

--- lib/test.php
+++ lib/test.php
@@ -5,9 +5,16 @@ include 'Factory.php';
 header('Content-type:text/html; charset=utf-8');
 $lib = Factory::createClass('LibJndx');
 if($lib->checkField("2011050659", "050659")){
-// 	var_dump($lib->renew('renew0-i3465229'));
-	var_dump($lib->renew('renew0-i3349282'));
+	var_dump($lib->renew('renew1-i3465229'));
 	var_dump($lib->getLoanList());
+	echo 1;
+}else{
+	echo 0;
+}
+die;
+if($lib->checkField("2011050663", "050663")){
+		var_dump($lib->renew('renew1-i3643976'));
+		var_dump($lib->getLoanList());
 	echo 1;
 }else{
 	echo 0;
